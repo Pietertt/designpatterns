f7b2e00c172162361075e95531068ca66e3507c7
Pieter Boersma <pieterboersma20@gmail.com>
2020-04-09 23:55:58 +0200

    Werkt beter

--- shapes/rectangle.java
+++ shapes/rectangle.java
@@ -26,28 +26,28 @@ public class Rectangle extends JPanel {
             this.height = height;
             System.out.println("Spawned");
 
-            // this.addMouseListener(new MouseAdapter() {
-            // public void mousePressed(MouseEvent e) {
-            // moveSquare(e.getX(),e.getY());
-            // }
-            // });
+            this.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+            moveSquare(e.getX(),e.getY());
+            }
+            });
 
-            // this.addMouseMotionListener(new MouseAdapter() {
-            // public void mouseDragged(MouseEvent e) {
-            // moveSquare(e.getX(),e.getY());
-            // }
-            // });
+            this.addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+            moveSquare(e.getX(),e.getY());
+            }
+            });
       }
 
-      // private void moveSquare(int x, int y) {
-      // int offset = 1;
-      // if ((this.x != x) || (this.y != y)) {
-      // repaint(this.x, this.y, this.width + offset, this.height + offset);
-      // this.x = x;
-      // this.y = y;
-      // repaint(this.x, this.y, this.width + offset, this.height + offset);
-      // }
-      // }
+      private void moveSquare(int x, int y) {
+      int offset = 1;
+      if ((this.x != x) || (this.y != y)) {
+      repaint(this.x, this.y, this.width + offset, this.height + offset);
+      this.x = x;
+      this.y = y;
+      repaint(this.x, this.y, this.width + offset, this.height + offset);
+      }
+      }
 
       // public Dimension getPreferredSize() {
       // return new Dimension(200, 200); // appropriate constants
