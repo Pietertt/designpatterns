92ffef0f52a2160c710e0799c678b6e1eae4ae35
Pieter Boersma <pieterboersma20@gmail.com>
2020-04-17 22:52:34 +0200

    Buttons toegevoegd

--- App.java
+++ App.java
@@ -1,20 +1,51 @@
+import java.awt.BorderLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
 public class App {
 
-      private static void init(){
-            JFrame frame = new JFrame();
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.add(new Board());
-            frame.pack();
-            frame.setVisible(true);
+      private static void init() {
+            try {
+                  JFrame frame = new JFrame();
+                  JPanel UI = new JPanel();
+                  JButton rectangle = new JButton();
+                  JButton ellipse = new JButton();
+
+                  rectangle.setIcon(new ImageIcon(ImageIO.read(new File("img/rectangle.png"))));
+                  ellipse.setIcon(new ImageIcon(ImageIO.read(new File("img/ellipse.png"))));
+
+                  rectangle.setBorderPainted(false);
+                  rectangle.setFocusPainted(false);
+                  rectangle.setContentAreaFilled(false);
+
+                  ellipse.setBorderPainted(false);
+                  ellipse.setFocusPainted(false);
+                  ellipse.setContentAreaFilled(false);
+
+                  UI.add(rectangle);
+                  UI.add(ellipse);
+
+
+                  frame.getContentPane().add(UI, BorderLayout.SOUTH);
+                  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                  frame.add(new Board());
+                  frame.pack();
+                  frame.setVisible(true);
+            } catch(IOException e){
+                  
+            }
       }
 
-      public static void main(String[] args){
+      public static void main(String[] args) {
             SwingUtilities.invokeLater(new Runnable() {
-                  public void run(){
+                  public void run() {
                         init();
                   }
             });
