d119b1fe53e812830d2ff5f3c26ccb9b8ab5c7ee
Pieter Boersma <pieterboersma20@gmail.com>
2020-04-25 18:52:35 +0200

    Rectangle tekenen naar het board overgeplaatst

--- UI/Board.java
+++ UI/Board.java
@@ -2,14 +2,53 @@ package UI;
 
 import javax.swing.JPanel;
 import java.awt.Dimension;
+import java.awt.event.*;
+import shapes.*;
 
-public class Board extends JPanel {
+public class Board extends JPanel implements MouseListener {
       public Board(){
             super(null);
+            addMouseListener(this);
+            setFocusable(true);
+
+            Rectangle rectangle1 = new Rectangle(100, 100, 50, 50);
+            add(rectangle1);
+
+            Rectangle rectangle2 = new Rectangle(400, 100, 50, 50);
+            add(rectangle2);
       }
 
       @Override
       public Dimension getPreferredSize() {
             return new Dimension(500, 500);
+      }
+
+      public void mouseClicked(MouseEvent e){
+            System.out.println("Click");
+      }
+
+      public void mouseExited(MouseEvent e){
+
+      }
+
+      public void mouseEntered(MouseEvent e){
+
+      } 
+
+      public void mouseReleased(MouseEvent e){
+
+      }
+
+      public void mousePressed(MouseEvent e){
+            requestFocus();
+                        
+      }
+
+      public void mouseDragged(MouseEvent e){
+
+      }
+
+      public void MouseMoved(MouseEvent e){
+
       }
 }
--- main.java
+++ main.java
@@ -9,30 +9,12 @@ import shapes.Rectangle;
 import UI.Board;
 
 public class main {
-
-      private Rectangle res;
-
+      
       public static void main(String[] args) {
             JFrame frame = new JFrame();
 
             Board board = new Board();
             frame.add(board);
-
-            Rectangle rectangle1 = new Rectangle(100, 100, 50, 50);
-            board.add(rectangle1);
-
-            Rectangle rectangle2 = new Rectangle(400, 100, 50, 50);
-            board.add(rectangle2);
-
-            frame.addMouseListener(new MouseAdapter() {
-                  @Override
-                  public void mousePressed(MouseEvent me) {
-
-                        frame.requestFocus();
-                        rectangle1.repaint();
-                        rectangle2.repaint();
-                  }
-            });
 
             frame.setLocationRelativeTo(null);
             frame.pack();
