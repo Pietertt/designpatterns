3efdbec71f0727af979f5f84217063a8cc2de666
Pieter Boersma <pieterboersma20@gmail.com>
2020-04-29 15:31:22 +0200

    FUCKCKCCKCK

--- visitor/moveVisitor.java
+++ visitor/moveVisitor.java
@@ -25,89 +25,88 @@ public class moveVisitor implements Visitor {
                               rectangle.invoker.execute(drag);
                               rectangle.dragging = false;
                         }  
-                        
-                        if(rectangle.resizing){
-                              Location location = new Location(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
-                              Order drag = new ResizeShapeCommand(rectangle, location);
-                              rectangle.invoker.execute(drag);
-                              rectangle.resizing = false;
-                        }
                   }
 
                   public void mousePressed(MouseEvent e){
+                        /*
+                              Executes a drag command to note the initial position
+                        */
                         if(rectangle.undoStack.size() == 0){
                               Location location = new Location(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
                               Order drag = new DragShapeCommand(rectangle, location);
                               rectangle.invoker.execute(drag);
                         }
-            
-                        if(!rectangle.selected){
-                              for(Shape shape : rectangle.board.shapes){
-                                    if(shape.selected){  
-                                          Order deselect = new DeselectShapeCommand(shape, e);
-                                          rectangle.invoker.execute(deselect);
-                                    }
-                              }
-            
-                              Order select = new SelectShapeCommand(rectangle, e);
-                              rectangle.invoker.execute(select);
-                              rectangle.requestFocus();
-                        }
-            
-                        var resizableBorder = (ResizableBorder) rectangle.getBorder();
-                        rectangle.cursor = resizableBorder.getCursor(e);
-                        rectangle.start = e.getPoint();
                   }
             });
 
             rectangle.addMouseMotionListener(new MouseAdapter(){
-                  public void mouseMoved(MouseEvent e){
-                        if (rectangle.selected) {
-                              var resizableBorder = (ResizableBorder) rectangle.getBorder();
-                              var cursor = resizableBorder.getCursor(e);
-                              rectangle.setCursor(Cursor.getPredefinedCursor(cursor));
-                        }
-                  }
-
                   public void mouseDragged(MouseEvent e){
                         if (rectangle.start != null) {
-                              int x = rectangle.getX();
-                              int y = rectangle.getY();
-                              int width = rectangle.getWidth();
-                              int height = rectangle.getHeight();
                               int dx = e.getX() - rectangle.start.x;
                               int dy = e.getY() - rectangle.start.y;
             
-                              switch (rectangle.cursor) {
-                                    case Cursor.SE_RESIZE_CURSOR:
-                                          rectangle.resizing = true;
-                                          rectangle.dragging = false;
-            
-                                          rectangle.setBounds(x, y, width + dx, height + dy);
-                                          rectangle.start = e.getPoint();
-                                          rectangle.width = width;
-                                          rectangle.height = height;
-                                          rectangle.repaint();
-                                          break;
-                                    case Cursor.MOVE_CURSOR:    
-                                          rectangle.resizing = false;  
-                                          rectangle.dragging = true;      
-            
-                                          var bounds = rectangle.getBounds();
-                                          bounds.translate(dx, dy);
-                                          rectangle.setBounds(bounds);
-                                          rectangle.width = rectangle.getWidth();
-                                          rectangle.height = rectangle.getHeight();
-                                          rectangle.repaint();
-            
+                              if(rectangle.cursor == Cursor.MOVE_CURSOR){
+                                    rectangle.resizing = false;  
+                                    rectangle.dragging = true;      
+
+                                    var bounds = rectangle.getBounds();
+                                    bounds.translate(dx, dy);
+                                    rectangle.setBounds(bounds);
+                                    rectangle.width = rectangle.getWidth();
+                                    rectangle.height = rectangle.getHeight();
+                                    rectangle.repaint();
                               }
-                              rectangle.setCursor(Cursor.getPredefinedCursor(rectangle.cursor));
                         }
                   }
             });
       }
 
       public void visitEllipse(Ellipse ellipse){
+            ellipse.addMouseListener(new MouseAdapter(){
+                  public void mouseReleased(MouseEvent e){
+                        /*
+                              Pollution of the undo/redo history is prevented by only adding 
+                              DragShapeCommand when the shape has been dragged
+                        */
+                        if(ellipse.dragging){
+                              Location location = new Location(ellipse.getX(), ellipse.getY(), ellipse.getWidth(), ellipse.getHeight());
+                              Order drag = new DragShapeCommand(ellipse, location);
+                              ellipse.invoker.execute(drag);
+                              ellipse.dragging = false;
+                        }  
+                  }
 
+                  public void mousePressed(MouseEvent e){
+                        /*
+                              Executes a drag command to note the initial position
+                        */
+                        if(ellipse.undoStack.size() == 0){
+                              Location location = new Location(ellipse.getX(), ellipse.getY(), ellipse.getWidth(), ellipse.getHeight());
+                              Order drag = new DragShapeCommand(ellipse, location);
+                              ellipse.invoker.execute(drag);
+                        }
+                  }
+            });
+
+            ellipse.addMouseMotionListener(new MouseAdapter(){
+                  public void mouseDragged(MouseEvent e){
+                        if (ellipse.start != null) {
+                              int dx = e.getX() - ellipse.start.x;
+                              int dy = e.getY() - ellipse.start.y;
+            
+                              if(ellipse.cursor == Cursor.MOVE_CURSOR){
+                                    ellipse.resizing = false;  
+                                    ellipse.dragging = true;      
+
+                                    var bounds = ellipse.getBounds();
+                                    bounds.translate(dx, dy);
+                                    ellipse.setBounds(bounds);
+                                    ellipse.width = ellipse.getWidth();
+                                    ellipse.height = ellipse.getHeight();
+                                    ellipse.repaint();
+                              }
+                        }
+                  }
+            });
       }
 }
