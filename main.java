8ebdaf6c3936716547a98f348163b3e80f001160
Pieter Boersma <pieterboersma20@gmail.com>
2020-04-30 10:34:20 +0200

    Een paar onnodige imports weg

--- main.java
+++ main.java
@@ -1,21 +1,9 @@
-import java.util.*;
 import javax.swing.*;
-
-import javax.swing.Timer;
-import java.awt.event.*;
-import java.awt.BorderLayout;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.awt.*;
-
-import java.awt.event.*;
 import java.awt.BorderLayout;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.io.File;
 
-import shapes.Rectangle;
 import UI.Board;
 import strategies.*;
 
