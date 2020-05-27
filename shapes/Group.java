d2e8659b6c9a8b8e8239e553ce27964d1e41ab2b
Pieter Boersma <pieterboersma20@gmail.com>
2020-05-27 22:18:07 +0200

    Comments

--- shapes/Group.java
+++ shapes/Group.java
@@ -196,7 +196,7 @@ public class Group extends BaseShape {
                   if(shape.selected){
                         Order deselect = new DeselectShapeCommand(shape, e);
                         deselect.execute();
-                        //this.board.invoker.execute(deselect);
+                        this.board.invoker.execute(deselect);
                   }
             }
 
@@ -205,25 +205,33 @@ public class Group extends BaseShape {
       }
 
       public void dragCommand(Location location){ 
+            // Clears the redo stack
             this.redoStack.clear();
+            // Adds a new item to the undo stack
             this.undoStack.add(location);
             this.dragging = true;
+            // Updates the start location
             this.start = new Location(location.x, location.y, location.width, location.height);
             repaint();
       }
 
       public void resizeCommand(Location location){
+            // Clears the redo stack
             this.redoStack.clear();
+            // Adds a new item to the undo stack
             this.undoStack.add(location);
             this.resizing = true;
+            // Updates the location
             this.start = new Location(location.x, location.y, location.width, location.height);
             repaint();
       }
 
       public void clear(){
+            // Clears the group
             this.dragging = false;
             this.resizing = false;
 
+            // Clears each child
             for(BaseShape shape : this.children){
                   Order save = new SaveShapeCommand(shape, new Location(shape.x, shape.y, shape.width, shape.height));
                   save.execute();
@@ -232,15 +240,19 @@ public class Group extends BaseShape {
       }
 
       public void undoDrag() {
+            // Pops an item from the undo stack
             Location location = this.undoStack.pop();
             Location locationOld = new Location(this.x, this.y, this.width, this.height);
             this.redoStack.add(locationOld);
+
+            // Assigns the popped item to the current x, y, width and height values
             this.x = location.x;
             this.y = location.y;
             this.width = location.width;
             this.height = location.height;
             repaint();
 
+            // Undos each child, if it exists
             if(this.children != null) {
                   for(BaseShape shape : this.children) {
                         shape.undoDrag();
@@ -250,9 +262,13 @@ public class Group extends BaseShape {
 
       public void redoDrag() {
             if (this.redoStack.size() > 0) {
+                  // Pops an item from the redo stack
                   Location location = this.redoStack.pop();
                   Location locationOld = new Location(this.x, this.y, this.width, this.height);
+                  // Adds the current x, y, width and height values to the undo stack
                   this.undoStack.add(locationOld);
+
+                  // Assigns the popped item to the current x, y, width and height values
                   this.x = location.x;
                   this.y = location.y;
                   this.width = location.width;
@@ -260,6 +276,7 @@ public class Group extends BaseShape {
                   repaint();
             }
 
+            // Redos each child, if it exists
             if(this.children != null) {
                   for(BaseShape shape : this.children) {
                         shape.redoDrag();
@@ -267,10 +284,13 @@ public class Group extends BaseShape {
             }
       }
 
+      // 'Prints' the group by displaying it on the right
       public void print(Layers layers){
+            // Sets the text of the label
             JLabel label = new JLabel();
             label.setText("Group");
             try {
+                  // Sets the icon of the label
                   ImageIcon image = new ImageIcon(ImageIO.read(new File("img/group.png")));
                   label.setIcon(image);
                   layers.add(Box.createRigidArea(new Dimension(10, 10)));
@@ -279,12 +299,14 @@ public class Group extends BaseShape {
 
             }
 
+            // 'Prints' each child
             for(BaseShape shape : this.children){
                   shape.print(layers);
                   
             }
       }
 
+      // Returns true whether a child is selected, returns false otherwise
       public boolean getIfSelected(int x, int y) {
             for(BaseShape shape : this.children){
                   if(shape.getIfSelected(x, y)){
@@ -294,8 +316,10 @@ public class Group extends BaseShape {
             return false;
       }
 
+      // Returns true when the handle is clicked
       public boolean getHandleIfSelected(int x, int y){
             boolean s = false;
+            // Returns true when a child's handle is selected
             for(BaseShape shape : this.children){
                   if(shape.getHandleIfSelected(x, y)){
                         s = true;
@@ -303,11 +327,15 @@ public class Group extends BaseShape {
                   }
             }
 
+            // Gets the group handle if no child handle is selected
             if(s == false){
+                  // Loops through the width of the handle
                   for(int i = this.x + this.width - 6; i < this.x + this.width + 6; i++){
+                        // Loops through the height of the handle
                         for(int j = this.y + this.height - 6; j < this.y + this.height + 6; j++){
                               if(i == x){
                                     if(j == y){
+                                          // Returns true when the handle is clicked
                                           return true;
                                     }
                               }
@@ -317,6 +345,7 @@ public class Group extends BaseShape {
             return false;
       }
 
+      // Returns the smallest x value of all children
       public int getx(){
             int x = this.children.get(0).x;
             for(BaseShape shape : this.children){
@@ -329,6 +358,7 @@ public class Group extends BaseShape {
             return x;
       }
 
+      // Returns the smallest y value of all children
       public int gety(){
             int y = this.children.get(0).y;
             for(BaseShape shape : this.children){
@@ -341,12 +371,17 @@ public class Group extends BaseShape {
             return y;
       }
 
+      // Returns the width of the group
       public int getwidth(){
             int maxWidth = 0;
             for(BaseShape shape : this.children){
+                  // Calculates the difference between the shape.x and the group.x 
                   int relative = shape.x - this.x;
+                  // Adds the calculated difference to the shape.width
                   int width = relative + shape.width;
                   if(shape.drawed){
+                        // Assigns the calculates max width to the width variable when is it larger
+                        // than the previous width
                         if(width > maxWidth){
                               maxWidth = width;
                         }
@@ -355,12 +390,17 @@ public class Group extends BaseShape {
             return maxWidth;
       }
 
+      // Returns the height of the group
       public int getheight(){
             int maxHeight = 0;
             for(BaseShape shape : this.children){
+                  // Calculates the difference between the shape.y and the group.y
                   int relative = shape.y - this.y;
+                  // Adds the calculated difference to the shape.height
                   int height = relative + shape.height;
                   if(shape.drawed){
+                        // Assigns the calculates max height to the height variable when is it larger
+                        // than the previous height
                         if(height > maxHeight){
                               maxHeight = height;
                         }
@@ -369,6 +409,7 @@ public class Group extends BaseShape {
             return maxHeight;
       }
 
+      // Determines whether a child is selected
       public boolean isChildSelected(){
             for(BaseShape shape : this.children){
                   if(shape.selected){
@@ -382,12 +423,15 @@ public class Group extends BaseShape {
       public void paintComponent(Graphics g) {
             super.paintComponent(g);                  
             
+            // Sets the group x, y, width and height attributes
             this.x = this.getx();
             this.y = this.gety();
             this.width = this.getwidth();
             this.height = this.getheight();
 
+            // The group must be selected at all times
             if(this.selected){
+                  // Draw the border when no child is selected
                   if(!this.isChildSelected()){
                         this.strategy.execute(this.x, this.y, this.width, this.height, g, true);
                   }
